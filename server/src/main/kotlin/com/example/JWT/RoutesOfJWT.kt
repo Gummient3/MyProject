81740228053ea22b6b7bfebf766726543991b688
Gummient3 <rikudo2703@gmail.com>
2024-12-15 02:08:59 +0300

    test Authorization

--- server/src/main/kotlin/com/example/JWT/RoutesOfJWT.kt
+++ server/src/main/kotlin/com/example/JWT/RoutesOfJWT.kt
@@ -2,14 +2,23 @@ package com.example.JWT
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.example.User.UserCreds
 import com.example.User.UserLog
 import com.example.User.UserLoginModel
 import com.example.secret
+import getRandomHexColor
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.html.respondHtml
+import io.ktor.server.http.httpDateFormat
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.head
+import kotlinx.html.style
 import net.mamoe.yamlkt.Yaml
 import java.io.File
 import java.util.Date
@@ -17,6 +26,24 @@ import java.util.Date
 
 
 fun Route.auth(){
+    post("/registration"){
+
+        val creds: UserCreds = call.receive<UserCreds>()
+        if (creds.password == "Admin123" && creds.username == "Admin"){
+            call.respondText(status = HttpStatusCode.OK){
+                "token-blA-B;A-F=-BLA-BLA-"
+            }
+        }
+        else{
+            call.respondText(status = HttpStatusCode.Unauthorized) {
+                """"POSHEL NAHUI"
+                ${creds.password}
+                ${creds.username} 
+                  """
+            }
+        }
+
+    }
     post("/login") {
         println(""" 
             +
@@ -31,13 +58,21 @@ fun Route.auth(){
             +
             +
         """.trimIndent())
+
         val user = call.receive<UserLog>()
 
         println(user.username)
-        call.respondText("""
+        if (user.username == "Admin" && user.password == "Admin123"){
+            call.respondText("""
             ${user.username}
             ${user.password}
+            ${HttpStatusCode.OK.toString()}
         """.trimIndent())
+        }
+        else{
+            call.respond("Unauthorized")
+        }
+
 
     }
 }
---
+++ server/src/main/kotlin/com/example/User/UserCreds.kt
@@ -0,0 +1,11 @@
+package com.example.User
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class UserCreds(val username: String, val password: String){
+
+
+
+}
